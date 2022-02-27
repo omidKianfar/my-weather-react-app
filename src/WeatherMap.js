297bddf54f81784abdb4edc3043fd3859e72a174
omid kianfar <kianfar.omid.990@gmail.com>
2022-02-27 00:43:19 -0800

    set city in state

--- src/WeatherMap.js
+++ src/WeatherMap.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 function App() {
+  // state ---------------------------------------
+  const [city, setCity] = useState("");
+
   return (
     <div>
       <main>
         <section>
-          <input type="text" placeholder="EnterCity" />
+          <input
+            type="text"
+            placeholder="EnterCity"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
         </section>
         <section>Weather detailes</section>
       </main>
