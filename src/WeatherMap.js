47b15fb7294c2e0dfca810af8970b5e06f5ebf5e
omid kianfar <kianfar.omid.990@gmail.com>
2022-02-27 00:41:39 -0800

    create input

---
+++ src/WeatherMap.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <main>
+        <section>
+          <input type="text" placeholder="EnterCity" />
+        </section>
+        <section>Weather detailes</section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
